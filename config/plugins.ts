1b5944cd08132647700450ab0893f2bb6b17bed1
Najib Slassi <mohamed-najib.slassi@mila.fr>
2025-12-27 10:26:07 +0100

    configure db and s3

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,6 +1,25 @@
-export default () => ({
+export default ({ env }) => ({
   newsletter: {
     enabled: true,
     resolve: "./src/plugins/newsletter",
+  },
+  upload: {
+    config: {
+      provider: "aws-s3",
+      providerOptions: {
+        s3Options: {
+          credentials: {
+            accessKeyId: env("S3_KEY", "strapi_user"),
+            secretAccessKey: env("S3_SECRET", "strapi_password"),
+          },
+          endpoint: env("S3_ENDPOINT", "http://127.0.0.1:9000"), // Local MinIO
+          forcePathStyle: true, // Required for MinIO
+          region: env("S3_REGION", "us-east-1"), // MinIO needs a placeholder region
+          params: {
+            Bucket: env("S3_BUCKET", "local-bucket"),
+          },
+        },
+      },
+    },
   },
 });
