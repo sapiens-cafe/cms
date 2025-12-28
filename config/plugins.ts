09b6f1a8ca6a5d921b4b4c10d8bf96db87d9b167
Najib Slassi <mohamed-najib.slassi@mila.fr>
2025-12-28 15:59:26 +0100

    try cloudinary

--- config/plugins.ts
+++ config/plugins.ts
@@ -3,37 +3,24 @@ export default ({ env }) => ({
     enabled: true,
     resolve: "./src/plugins/newsletter",
   },
-  upload: {
-    config: {
-      provider: "aws-s3",
-      providerOptions: {
-        // base urls is only for local development with MinIO
-        // it should be omitted or set to undefined in production
-        baseUrl: env("S3_BASE_URL"),
-        s3Options: {
-          credentials: {
-            accessKeyId: env("S3_KEY", "strapi_user"),
-            secretAccessKey: env("S3_SECRET", "strapi_password"),
-          },
-          endpoint: env("S3_ENDPOINT", "http://localhost:9000"), // Local MinIO
-          forcePathStyle: true, // Required for MinIO
-          region: env("S3_REGION", "us-east-1"), // MinIO needs a placeholder region
-          params: {
-            Bucket: env("S3_BUCKET", "local-bucket"),
-            ACL: "private", // <== set ACL to private
-            signedUrlExpires: env("AWS_SIGNED_URL_EXPIRES", 15 * 60),
+
+  ...(env("NODE_ENV") === "production"
+    ? {
+        upload: {
+          config: {
+            provider: "cloudinary",
+            providerOptions: {
+              cloud_name: env("CLOUDINARY_NAME"),
+              api_key: env("CLOUDINARY_KEY"),
+              api_secret: env("CLOUDINARY_SECRET"),
+            },
+            actionOptions: {
+              upload: {},
+              uploadStream: {},
+              delete: {},
+            },
           },
         },
-        actionOptions: {
-          upload: {},
-          uploadStream: {},
-          delete: {},
-          // Cette ligne force la génération d'URLs signées pour l'affichage
-          getSignedUrl: {
-            expiresIn: 3600, // Durée de validité en secondes (1h)
-          },
-        },
-      },
-    },
-  },
+      }
+    : {}),
 });
