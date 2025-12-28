1784bcdbd5b23f36125001cf6c94bb27cf4cb5c8
Najib Slassi <mohamed-najib.slassi@mila.fr>
2025-12-28 12:20:01 +0100

    configure lcoal minio

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,13 +1,40 @@
 export default [
-  'strapi::logger',
-  'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
-  'strapi::poweredBy',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
-  'global::deepPopulate'
+  "strapi::logger",
+  "strapi::errors",
+  {
+    name: "strapi::security",
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          "connect-src": ["'self'", "https:", "http:"],
+          "img-src": [
+            "'self'",
+            "data:",
+            "blob:",
+            "market-assets.strapi.io",
+            "localhost:9000",
+            "127.0.0.1:9000",
+          ],
+          "media-src": [
+            "'self'",
+            "data:",
+            "blob:",
+            "market-assets.strapi.io",
+            "localhost:9000",
+            "127.0.0.1:9000",
+          ],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
+  "strapi::cors",
+  "strapi::poweredBy",
+  "strapi::query",
+  "strapi::body",
+  "strapi::session",
+  "strapi::favicon",
+  "strapi::public",
+  "global::deepPopulate",
 ];
--- config/plugins.ts
+++ config/plugins.ts
@@ -7,12 +7,13 @@ export default ({ env }) => ({
     config: {
       provider: "aws-s3",
       providerOptions: {
+        baseUrl: env("S3_BASE_URL", "http://localhost:9000/local-bucket"),
         s3Options: {
           credentials: {
             accessKeyId: env("S3_KEY", "strapi_user"),
             secretAccessKey: env("S3_SECRET", "strapi_password"),
           },
-          endpoint: env("S3_ENDPOINT", "http://127.0.0.1:9000"), // Local MinIO
+          endpoint: env("S3_ENDPOINT", "http://localhost:9000"), // Local MinIO
           forcePathStyle: true, // Required for MinIO
           region: env("S3_REGION", "us-east-1"), // MinIO needs a placeholder region
           params: {
